9d5bc64c21b3b1dba5ba74c4671c1ac241d4caf6
y-n-h-y <ec2-user@ip-172-31-12-148.ec2.internal>
2021-08-25 16:09:49 +0000

    addition header top about signup signin

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,18 @@
 class BooksController < ApplicationController
   
   def index
+    @book = Book.new
+    @books = Book.all
   end
   
   def show
   end
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book.id)
   end
   
   def edit
@@ -16,6 +22,12 @@ class BooksController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
