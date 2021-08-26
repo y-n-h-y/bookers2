855b2ecbf3e9dc49ca4d69bb402457159c99a062
y-n-h-y <ec2-user@ip-172-31-12-148.ec2.internal>
2021-08-26 16:27:26 +0000

    add layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
